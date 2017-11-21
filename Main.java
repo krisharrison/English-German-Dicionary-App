9ef40e27116bd3da222b0ba6365a4ca75989f34a
KrisHarrison <33849103+KrisHarrison@users.noreply.github.com>
2017-11-21 00:13:41 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,41 @@
+/**
+ * This class sets the primary stage
+ */
+
+package finalproject;
+
+
+/**
+ *  Final Project
+ * Thursday, August 10, 2017
+ * @author Kris Harrison
+ * 
+ */
+
+//IMPORTS
+import javafx.application.Application;
+import static javafx.application.Application.launch;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.fxml.FXMLLoader;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
